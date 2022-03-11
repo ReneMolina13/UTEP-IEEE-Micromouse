72efac4409be8e600b3363cdc3e03b438fbb414a
amartinezacosta <amartinezacosta@miners.utep.edu>
2022-03-11 10:40:12 -0700

    added src folder with micromouse and maze code

---
+++ src/maze.cpp
@@ -0,0 +1,200 @@
+#include "maze.h"
+
+/*Virtual mouse object*/
+mouse_t mouse = {.x = 0, .y = 0, .heading = NORTH};
+
+/*Maze cells array*/
+cell_t maze[MAZE_WIDTH][MAZE_HEIGHT];
+
+int8_t maze_set_wall(uint8_t x, uint8_t y, uint8_t wall)
+{
+  if(CHECK_BOUNDARIES(x, y)) return -1;
+  
+  maze[x][y].walls |= wall;
+
+  switch(wall)
+  {
+    case NORTH:
+      maze[x][y-1].walls |= SOUTH;
+      break;
+    case EAST:
+      maze[x+1][y].walls |= WEST;
+      break;
+    case SOUTH: 
+      maze[x][y+1].walls |= NORTH;
+      break;
+    case WEST:
+      maze[x-1][y].walls |= EAST;
+      break; 
+  }
+}
+
+int8_t maze_get_walls(uint8_t x, uint8_t y)
+{
+  if(CHECK_BOUNDARIES(x, y)) return -1;
+   
+  return maze[x][y].walls;
+}
+
+int8_t maze_set_value(uint8_t x, uint8_t y, uint8_t value)
+{
+  if(CHECK_BOUNDARIES(x, y)) return -1;
+  
+  maze[x][y].value = value;
+}
+
+int8_t maze_get_value(uint8_t x, uint8_t y)
+{
+  if(CHECK_BOUNDARIES(x, y)) return -1;
+
+   return maze[x][y].value;
+}
+
+void maze_init(uint8_t target_x, uint8_t target_y)
+{
+  if(CHECK_BOUNDARIES(target_x, target_y)) return -1;
+  
+  for(uint8_t y = 0; y < MAZE_HEIGHT; y++)
+  {
+    for(uint8_t x = 0; x < MAZE_WIDTH; x++)
+    {
+      maze[x][y].value = abs(target_x - x) + abs(target_y - y);
+      maze[x][y].pos = X_POS(x) | Y_POS(y);  
+
+      if(x == 0)                 maze[x][y].walls |= WEST;
+      if(x == (MAZE_WIDTH - 1))  maze[x][y].walls |= EAST;
+      if(y == 0)                 maze[x][y].walls |= NORTH;
+      if(y == (MAZE_HEIGHT - 1)) maze[x][y].walls |= SOUTH;
+    }
+  }
+}
+
+/*Print routine for VT100 terminals. Recommended terminal is putty*/
+void maze_print(void)
+{
+  /*Clear previous maze*/
+  Serial.print("\x1B[2J");
+  
+  for(uint8_t y = 0; y < MAZE_HEIGHT; y++)
+  {
+    for(uint8_t x = 0; x < MAZE_WIDTH; x++)
+    {
+      uint8_t cy = 4*x + 1;
+      uint8_t cx = 2*y + 1;
+
+      /*Print cell value*/
+      Serial.print("\x1B[");
+      Serial.print(cx+1);
+      Serial.print(";");
+      Serial.print(cy+2);
+      Serial.print("H");
+      Serial.print(maze[x][y].value, DEC);
+
+      /*Print north wall if present*/
+      if(maze[x][y].walls & NORTH)
+      {
+        Serial.print("\x1B[");
+        Serial.print(cx);
+        Serial.print(";");
+        Serial.print(cy);
+        Serial.print("H");
+        Serial.print("+---+");
+      }
+      
+      /*Print east wall if present*/
+      if(maze[x][y].walls & EAST)
+      {
+        Serial.print("\x1B[");
+        Serial.print(cx+1);
+        Serial.print(";");
+        Serial.print(cy+4);
+        Serial.print("H");
+        Serial.print("|");
+      }
+
+      /*Print south wall if present*/
+      if(maze[x][y].walls & SOUTH)
+      {
+        Serial.print("\x1B[");
+        Serial.print(cx+2);
+        Serial.print(";");
+        Serial.print(cy);
+        Serial.print("H");
+        Serial.print("+---+");
+      }
+
+      /*Print west wall if present*/
+      if(maze[x][y].walls & WEST)
+      {
+        Serial.print("\x1B[");
+        Serial.print(cx+1);
+        Serial.print(";");
+        Serial.print(cy);
+        Serial.print("H");
+        Serial.print("|");
+      }
+    }
+  }
+
+  /*Print mouse's position and heading*/
+  uint8_t mx = 2*mouse.y + 1;
+  uint8_t my = 4*mouse.x + 1;
+
+  Serial.print("\x1B[");
+  Serial.print(mx+1);
+  Serial.print(";");
+  Serial.print(my+2);
+  Serial.print("H");
+
+  switch(mouse.heading)
+  {
+    case NORTH:
+      Serial.print("^");
+      break;
+    case EAST:
+      Serial.print(">");
+      break;
+    case SOUTH:
+      Serial.print("V");
+      break;
+    case WEST:
+      Serial.print("<");
+      break;
+  }
+}
+
+void mouse_set_x(int8_t x)
+{
+  if(x > (MAZE_WIDTH - 1)) { x = MAZE_WIDTH - 1; }
+  if(x < 0) { x = 0; }
+  
+  mouse.x = x;
+}
+
+void mouse_set_y(int8_t y)
+{
+  if(y > (MAZE_HEIGHT - 1)) { y = MAZE_HEIGHT - 1; }
+  if(y < 0) { y = 0; }
+  
+  mouse.y = y;
+}
+
+int8_t mouse_get_x(void)
+{
+  return mouse.x;
+}
+
+int8_t mouse_get_y(void)
+{
+  return mouse.y;
+}
+
+void mouse_set_heading(uint8_t heading)
+{
+  mouse.heading = heading;
+}
+
+uint8_t mouse_get_heading(void)
+{
+   return mouse.heading;
+}
---
+++ src/micromouse.h
@@ -0,0 +1,46 @@
+#ifndef MICROMOUSE_H_
+#define MICROMOUSE_H_
+
+#include <Arduino.h>
+#include <stdbool.h>
+
+/*Motor definitions*/
+#define ENABLE            A6
+#define MOTOR_0_1A        11
+#define MOTOR_0_2A        5
+#define MOTOR_1_1A        6  
+#define MOTOR_1_2A        9
+#define ENCODER_0_CHA     2
+#define ENCODER_0_CHB     13
+#define ENCODER_1_CHA     3
+#define ENCODER_1_CHB     8
+
+/*Infrared sensors definitions*/
+#define IR0_TX       A1
+#define IR1_TX       12
+#define IR2_TX       7
+#define IR3_TX       4
+#define IR0_RX       A0
+#define IR1_RX       A7
+#define IR2_RX       A2
+#define IR3_RX       A3
+
+/*Extras*/
+#define BUTTON      10
+
+void motors_init(void);
+void motor_0_speed(int speed, bool direction);
+void motor_1_speed(int speed, bool direction);
+
+void ir_init(void);
+int ir_0_read(int delay);
+int ir_1_read(int delay);
+int ir_2_read(int delay);
+int ir_3_read(int delay);
+
+void gyro_init(void);
+
+void control_forward(void);
+void control_turn(float target_angle);
+
+#endif
